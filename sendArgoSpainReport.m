345849dac38b9140996d854212a783b06c5027a6
Pedro Velez <pvelezbelchi@gmail.com>
2023-09-05 10:41:35 +0200

    udpate .dats

--- sendArgoSpainReport.m
+++ sendArgoSpainReport.m
@@ -14,7 +14,7 @@ R4=load(strcat(PaginaWebDir,'/data/reportArgoInterestStatus'));
 
 if sendEmail==1
 try
-EnviaCorreoArgo('pedro.velez@ieo.es',sprintf('Web actualizada %s',datestr(now)),sprintf('%s\n\n%s\n\n%s\n\n%s\n\nhttp://www.argoespana.es',R1.Informe,R2.Informe,R3.Informe,R4.Informe))
+EnviaCorreoArgo('pvelezbelchi@gmail.com',sprintf('Web actualizada %s',datestr(now)),sprintf('%s\n\n%s\n\n%s\n\n%s\n\nhttp://www.argoespana.es',R1.Informe,R2.Informe,R3.Informe,R4.Informe))
 catch ME
     MensajeError=sprintf('Error al enviar el informe tras actuaizar la web el %s',datestr(now)),sprintf('EnviaInforme - %s line %d',ME.message,ME.stack(1).line,datestr(now));
     EnviaCorreoArgo('pedro.velez@ieo.es',MensajeError)
