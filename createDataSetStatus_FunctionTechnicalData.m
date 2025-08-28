09ad7225591b83062d49a2d1cfcbf810d009e91d
Pedro Velez <pvelezbelchi@gmail.com>
2025-08-28 09:18:22 +0000

    added CTD0 legend

--- createDataSetStatus_FunctionTechnicalData.m
+++ createDataSetStatus_FunctionTechnicalData.m
@@ -1,7 +1,31 @@
 fprintf(' technical data, ')
+
+%Voltage
+if isfield(FloatData.TEDf,'VOLTAGE_BatteryParkEnd_VOLTS')==1
+    hC1=axes('position',[0.05 0.69 0.90 0.26]);
+    plot(FloatData.TEDf.VOLTAGE_BatteryParkEnd_VOLTSCycle,FloatData.TEDf.VOLTAGE_BatteryParkEnd_VOLTS)
+    set(hC1,'fontsize',8)
+    title('VOLTAGE BatteryParkEnd VOLTS')
+    grid on
+    hC1.FontSize=7;
+    hC1.TitleFontWeight='normal';
+    hC1.XColor=[0.5 0.5 0.5];
+    hC1.YColor=[0.5 0.5 0.5];
+elseif isfield(FloatData.TEDf,'VOLTAGE_BatteryPumpStartProfile_volts')==1
+    hC1=axes('position',[0.05 0.69 0.90 0.26]);
+    plot(FloatData.TEDf.VOLTAGE_BatteryPumpStartProfile_voltsCycle,FloatData.TEDf.VOLTAGE_BatteryPumpStartProfile_volts)
+    set(hC1,'fontsize',8)
+    title('VOLTAGE BatteryPumpStartProfile volts')
+    grid on
+    hC1.FontSize=7;
+    hC1.TitleFontWeight='normal';
+    hC1.XColor=[0.5 0.5 0.5];
+    hC1.YColor=[0.5 0.5 0.5];
+end
+
 %Surface Pressure
 if isfield(FloatData.TEDf,'PRES_SurfaceOffsetTruncatedplus5dBar_dBAR')==1 && length(FloatData.TEDf.PRES_SurfaceOffsetTruncatedplus5dBar_dBAR)>1
-    hC1=axes('position',[0.05 0.69 0.90 0.26]);
+    hC2=axes('position',[0.05 0.36 0.90 0.26]);
     plot(FloatData.TEDf.PRES_SurfaceOffsetTruncatedplus5dBar_dBARCycle,FloatData.TEDf.PRES_SurfaceOffsetTruncatedplus5dBar_dBAR)
     maxSO=nanmax(FloatData.TEDf.PRES_SurfaceOffsetTruncatedplus5dBar_dBAR);
     minSO=nanmin(FloatData.TEDf.PRES_SurfaceOffsetTruncatedplus5dBar_dBAR);
@@ -11,13 +35,13 @@ if isfield(FloatData.TEDf,'PRES_SurfaceOffsetTruncatedplus5dBar_dBAR')==1 && len
         axis([-inf inf minSO maxSO])
     end
     grid on
-    hC1.FontSize=7;
-    hC1.TitleFontWeight='normal';
-    hC1.XColor=[0.5 0.5 0.5];
-    hC1.YColor=[0.5 0.5 0.5];
+    hC2.FontSize=7;
+    hC2.TitleFontWeight='normal';
+    hC2.XColor=[0.5 0.5 0.5];
+    hC2.YColor=[0.5 0.5 0.5];
     title('PRES SurfaceOffsetTruncatedplus5dBar dBAR')
 elseif isfield(FloatData.TEDf,'PRES_SurfaceOffsetNotTruncated_dBAR')==1
-    hC1=axes('position',[0.05 0.69 0.90 0.26]);
+    hC2=axes('position',[0.05 0.36 0.90 0.26]);
     plot(FloatData.TEDf.PRES_SurfaceOffsetNotTruncated_dBARCycle,FloatData.TEDf.PRES_SurfaceOffsetNotTruncated_dBAR);hold on
     maxSO=nanmax(FloatData.TEDf.PRES_SurfaceOffsetNotTruncated_dBAR);
     minSO=nanmin(FloatData.TEDf.PRES_SurfaceOffsetNotTruncated_dBAR);
@@ -27,34 +51,13 @@ elseif isfield(FloatData.TEDf,'PRES_SurfaceOffsetNotTruncated_dBAR')==1
         axis([-inf inf minSO maxSO])
     end
     grid on
-    hC1.FontSize=7;
-    hC1.TitleFontWeight='normal';
-    hC1.XColor=[0.5 0.5 0.5];
-    hC1.YColor=[0.5 0.5 0.5];
+    hC2.FontSize=7;
+    hC2.TitleFontWeight='normal';
+    hC2.XColor=[0.5 0.5 0.5];
+    hC2.YColor=[0.5 0.5 0.5];
     title('PRES SurfaceOffsetNotTruncated dBAR')
 end
-%Voltage
-if isfield(FloatData.TEDf,'VOLTAGE_BatteryParkEnd_VOLTS')==1
-    hC2=axes('position',[0.05 0.36 0.90 0.26]);
-    plot(FloatData.TEDf.VOLTAGE_BatteryParkEnd_VOLTSCycle,FloatData.TEDf.VOLTAGE_BatteryParkEnd_VOLTS)
-    set(hC2,'fontsize',8)
-    title('VOLTAGE BatteryParkEnd VOLTS')
-    grid on
-    hC2.FontSize=7;
-    hC2.TitleFontWeight='normal';
-    hC2.XColor=[0.5 0.5 0.5];
-    hC2.YColor=[0.5 0.5 0.5];
-elseif isfield(FloatData.TEDf,'VOLTAGE_BatteryPumpStartProfile_volts')==1
-    hC2=axes('position',[0.05 0.36 0.90 0.26]);
-    plot(FloatData.TEDf.VOLTAGE_BatteryPumpStartProfile_voltsCycle,FloatData.TEDf.VOLTAGE_BatteryPumpStartProfile_volts)
-    set(hC2,'fontsize',8)
-    title('VOLTAGE BatteryPumpStartProfile volts')
-    grid on
-    hC2.FontSize=7;
-    hC2.TitleFontWeight='normal';
-    hC2.XColor=[0.5 0.5 0.5];
-    hC2.YColor=[0.5 0.5 0.5];
-end
+
 %Presion interna
 if isfield(FloatData.TEDf,'PRESSURE_InternalVacuum_COUNT')==1
     hC3=axes('position',[0.05 0.05 0.90 0.26]);
